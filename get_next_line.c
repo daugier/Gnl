35c9b679de10eb85bb93c2cc4c7aabf7d2b54867
daugier <daugier@students.42.fr>
2016-05-21 22:48:27 +0200

    Presque Presque

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,13 @@
 /*   By: daugier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/29 15:48:42 by daugier           #+#    #+#             */
-/*   Updated: 2016/05/21 19:21:45 by daugier          ###   ########.fr       */
+/*   Updated: 2016/05/21 22:47:33 by daugier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-int		read_next(int fd, char **buffer)
+static int		read_next(int fd, char **buffer)
 {
 	int		ret;
 	char	*buf;
@@ -23,14 +22,11 @@ int		read_next(int fd, char **buffer)
 	if (!(buf = (char*)malloc(sizeof(char) * BUF_SIZE + 1)))
 		return (0);
 	while ((ret = read(fd, buf, BUF_SIZE)))
-	{
-		buf[ret] = '\0';
 		*buffer = ft_strjoin(*buffer, buf);
-	}
 	return (ret);
 }
 
-int		get_next_line(int const fd, char **line)
+int				get_next_line(int const fd, char **line)
 {
 	static char		*buffer[MAX_FD];
 
@@ -41,9 +37,7 @@ int		get_next_line(int const fd, char **line)
 		if (read_next(fd, &buffer[fd]) == -1)
 			return (-1);
 	}
-	if (!(*line = (char*)malloc(sizeof(char) * 1)))
-		return (0);
-	if (!*buffer[fd])
+	if (!(*line = (char*)malloc(sizeof(char) * 1)) || !*buffer[fd])
 		return (0);
 	while (*buffer[fd] != '\n' && *buffer[fd] != '\0')
 	{
