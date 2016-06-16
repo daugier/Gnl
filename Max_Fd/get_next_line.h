5be61f2001f17620c4061538b512009791fdc22a
daugier <daugier@students.42.fr>
2016-06-16 16:29:10 +0200

    Max Fd

---
+++ Max_Fd/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daugier <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/29 15:40:39 by daugier           #+#    #+#             */
+/*   Updated: 2016/06/10 22:48:26 by daugier          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUF_SIZE 1000
+# define MAX_FD 256
+# define MIN_FD 0
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft/includes/libft.h"
+
+int					get_next_line(int const fd, char **line);
+
+#endif
